05de8f87806d19e695e8dad4dcacac888db03178
Sean Murphy <filmmakersean@fastmail.net>
2019-08-18 20:28:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description : project.css("p.bbcard_blurb").text
   # location : project.css("ul.project-meta li a span").first
-  #percent funded : 
+  #percent funded : project.css("ul.project-stats li.first.funded strong").text
   
   binding.pry
 end
